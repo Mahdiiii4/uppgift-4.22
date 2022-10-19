fb9dae9182946acaa21fec706e1926b3271182dd
Admin <Admin@TEB123003>
2022-10-19 11:50:33 +0200

    Add project files.

---
+++ loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         for(int i = 10; i <= 30; i++)
+
+             Console.WriteLine(i);
+
+         for (int j = 200; j >= 180; j--) 
+
+                Console.WriteLine(j--);
+
+         for (int g=1000; g<=1400; g+=50)
+
+                Console.WriteLine(g);
+        }
+    }
+}
